603b0b6fa5b959c05869b6c72b7c3b3836692354
darkblueeggplant <darkblueeggplant@example.com>
2024-01-12 19:24:11 +0300

    hw04 try#1

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -1,6 +1,41 @@
 package main
 
+import "fmt"
+
+type Book struct {
+	id     int32
+	title  string
+	author string
+	year   int32
+	size   int32
+	rate   float32
+}
+
 func main() {
 	// Place your code here.
-	//branch
+
+	firstBook := Book{}
+	secondBook := Book{}
+	firstBook.create(1, "Harry Potter and the Philosopher's Stone", "J. K. Rowling", 1997, 300, 0.7)
+	fmt.Println(firstBook)
+	secondBook.create(2, "Harry Potter and the Chamber of Secrets", "J. K. Rowling", 1998, 300, 0.5)
+	fmt.Println(secondBook)
+	// fmt.Println(firstBook.title)
+	// fmt.Println(secondBook.title)
+
+	fmt.Println(firstBook.get())
+	fmt.Println(secondBook.get())
+}
+
+func (f *Book) create(id int32, title string, author string, year int32, size int32, rate float32) {
+	f.id = id
+	f.title = title
+	f.author = author
+	f.year = year
+	f.size = size
+	f.rate = rate
+}
+
+func (f *Book) get() (int32, string, string, int32, int32, float32) {
+	return f.id, f.title, f.author, f.year, f.size, f.rate
 }
