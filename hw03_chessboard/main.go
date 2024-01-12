0cd09c5ab212c7e77472f8e7a4500fcf93373dca
darkblueeggplant <darkblueeggplant@example.com>
2024-01-12 13:43:35 +0300

    hw03_chessboard add logic

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Place your code here.
+	fmt.Println("Please, enter board size")
+	var size int
+	fmt.Scanf("%d", &size)
+
+	chessboard(size)
+}
+
+func chessboard(size int) {
+	black := "#"
+	white := " "
+	finalblack := ""
+	finalwhite := ""
+	for i := 0; i < size; i++ {
+		if i%2 == 0 {
+			finalblack = finalblack + black
+			finalwhite = finalwhite + white
+		} else {
+			finalblack = finalblack + white
+			finalwhite = finalwhite + black
+		}
+	}
+	// fmt.Println(finalblack)
+	// fmt.Println(finalwhite)
+	for i := 0; i < size; i++ {
+		if i%2 == 0 {
+			fmt.Println(finalblack)
+		} else {
+			fmt.Println(finalwhite)
+		}
+	}
 }
