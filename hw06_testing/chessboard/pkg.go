d1fd82f4be1413a25292514c1f886245eee672e4
darkblueeggplant <darkblueeggplant@example.com>
2024-02-20 13:06:43 +0300

    project struct

---
+++ hw06_testing/chessboard/pkg.go
@@ -0,0 +1,8 @@
+package chessboard
+
+import "fmt"
+
+func PrintStaff() string {
+	str := "Hello,dummy!"
+	fmt.Println(str)
+}
---
+++ hw06_testing/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/darkblueeggplant/home_work_basic/chessboard"
+)
+
+func main() {
+	chessboard.PrintStaff()
+}
