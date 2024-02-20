6d90349569c11bd910fe43f2317ef981d7ef30c0
darkblueeggplant <darkblueeggplant@example.com>
2024-02-20 11:46:18 +0300

    errors, go mod

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 type Shape interface {
-	calculateArea() float64
+	calculateArea() (float64, error)
 }
 
 type Circle struct {
@@ -18,34 +18,89 @@ type Triangle struct {
 	side   float64
 }
 
-func (c Circle) calculateArea() float64 {
-	return 3.14 * c.radius * c.radius
+type BadShape struct{}
+
+func (b BadShape) calculateArea() (float64, error) {
+	return 0, fmt.Errorf("переданный объект не является фигурой")
 }
 
-func (r Rectangle) calculateArea() float64 {
-	return r.width * r.height
+func (c Circle) calculateArea() (float64, error) {
+	if c.radius < 0 {
+		return 0, fmt.Errorf("задан отрицательный радиус")
+	}
+	return 3.14 * c.radius * c.radius, nil
 }
 
-func (t Triangle) calculateArea() float64 {
-	return 0.5 * t.height * t.side
+func (r Rectangle) calculateArea() (float64, error) {
+	if r.height < 0 || r.width < 0 {
+		return 0, fmt.Errorf("задано отрицательное значение одной или обеих сторон")
+	}
+	return r.width * r.height, nil
 }
 
-func calculateArea(shape Shape) float64 {
-	return shape.calculateArea()
+func (t Triangle) calculateArea() (float64, error) {
+	if t.height < 0 || t.side < 0 {
+		return 0, fmt.Errorf("задано отрицательное значение основания или высоты")
+	}
+	return 0.5 * t.height * t.side, nil
+}
+
+func calculateArea(shape Shape) (float64, error) {
+	// return shape.calculateArea()
+	area, err := shape.calculateArea()
+	if err != nil {
+		return 0, err
+	}
+	return area, nil
 }
 
 func main() {
 	// Place your code here.
 	circle := Circle{radius: 2}
 	fmt.Println("круг: радиус", circle.radius)
-	fmt.Println("площадь круга:", calculateArea(circle))
+
+	circleArea, err := calculateArea(circle)
+	if err != nil {
+		fmt.Println("не могу посчитать площадь: ", err)
+	} else {
+		fmt.Println("площадь: ", circleArea)
+	}
+
+	circleBad := Circle{radius: -3}
+	fmt.Println("круг: радиус", circleBad.radius)
+
+	circleBadArea, err := calculateArea(circleBad)
+	if err != nil {
+		fmt.Println("не могу посчитать площадь: ", err)
+	} else {
+		fmt.Println("площадь: ", circleBadArea)
+	}
 
 	rectangle := Rectangle{width: 3, height: 2}
 	fmt.Println("прямоугольник: ширина", rectangle.width, "высота:", rectangle.height)
-	fmt.Println("площадь прямоугольника:", calculateArea(rectangle))
+
+	rectangleArea, err := calculateArea(rectangle)
+	if err != nil {
+		fmt.Println("не могу посчитать площадь: ", err)
+	} else {
+		fmt.Println("площадь: ", rectangleArea)
+	}
 
 	triangle := Triangle{height: 10, side: 3}
 	fmt.Println("треуголльник: высота", triangle.height, "основание:", triangle.side)
-	fmt.Println("площадь треугольника:", calculateArea(triangle))
 
+	triangleArea, err := calculateArea(triangle)
+	if err != nil {
+		fmt.Println("не могу посчитать площадь: ", err)
+	} else {
+		fmt.Println("площадь: ", triangleArea)
+	}
+
+	invalidShape := BadShape{}
+	invalidShapeArea, err := calculateArea(invalidShape)
+	if err != nil {
+		fmt.Println("не могу посчитать площадь: ", err)
+	} else {
+		fmt.Println("площадь: ", invalidShapeArea)
+	}
 }
