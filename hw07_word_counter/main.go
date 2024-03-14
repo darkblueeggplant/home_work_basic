1b6d5d105f82a4def6b848100aedec21973f5efc
darkblueeggplant <darkblueeggplant@yandex.ru>
2024-03-14 14:32:15 +0300

    logic

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+func countWords(text string) map[string]int {
+	words := strings.Fields(text) //  функции для работы со строками, чтобы разделить текст на отдельные слова
+	// println(len(words)) 7
+	wordCounts := make(map[string]int)
+	for _, word := range words {
+		wordCounts[word]++ //
+		// println(word)
+	}
+	return wordCounts
+}
+
 func main() {
-	// Place your code here.
+	text := "рельсы рельсы шпалы шпалы едет поезд запоздалый"
+	wordCountMap := countWords(text)
+	fmt.Println(wordCountMap)
 }
